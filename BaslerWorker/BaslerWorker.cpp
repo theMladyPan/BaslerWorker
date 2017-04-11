2bc4ec3eb31f6c956906121a5a548b656a55fc79
Stanke <stanislav@rubint.sk>
2017-04-11 16:56:00 +0200

    minor improvements

--- BaslerWorker/BaslerWorker.cpp
+++ BaslerWorker/BaslerWorker.cpp
@@ -8,7 +8,7 @@
 
 #include <pylon/PylonIncludes.h>
 #ifdef PYLON_WIN_BUILD
-#    include <pylon/PylonGUI.h>
+	#include <pylon/PylonGUI.h>
 #endif
 
 using namespace Pylon;
@@ -17,6 +17,8 @@ using namespace libconfig;
 
 // Number of images to be grabbed.
 static const uint32_t c_countOfImagesToGrab = 2;
+static const size_t c_maxCamerasToUse = 2;
+
 
 struct kamera {
 	CInstantCamera camera;
@@ -24,7 +26,6 @@ struct kamera {
 	unsigned char gain;
 	string ID;
 	string serial;
-
 };
 
 int main(int argc, char* argv[])
@@ -34,33 +35,58 @@ int main(int argc, char* argv[])
 
 	// Before using any pylon methods, the pylon runtime must be initialized. 
 	PylonInitialize();
-	kamera kamery[8];
+
+	//vytvorit a analyzovat dostupne kamery
+	CTlFactory& tlfactory = CTlFactory::GetInstance();
+	DeviceInfoList_t devices;
+	tlfactory.EnumerateDevices(devices);
+	if (!devices.empty()) { //ak nie je prazdne pole:
+		DeviceInfoList_t::const_iterator it;
+		for (it = devices.begin(); it != devices.end(); ++it)
+			cout << it->GetFullName() << " found!" << endl << endl; //vypis nazov
+	}
+	else //inak vypis chybu
+		cerr << "No devices found!" << endl;
+
+	/*kamera kamery[8];
 	for (unsigned char i; i < 8; i++) {
-		kamery[i].camera.Attach(CTlFactory::GetInstance().CreateFirstDevice());
+		kamery[i].camera.Attach(CTlFactory::GetInstance().CreateDevice());
+		kamery[i].serial = kamery[i].camera.GetDeviceInfo().GetSerialNumber();
+		cout << "found: " << kamery[i].serial << endl;
+	}*/
+
+
+	//vytvor pole kamier (max c_maxcamerastouse)
+	CInstantCameraArray kamery(min(devices.size(), c_maxCamerasToUse));
+
+	//napln ich
+	for (size_t i = 0; i < kamery.GetSize(); ++i)
+	{
+		kamery[i].Attach(tlfactory.CreateDevice(devices[i]));
 	}
 
 	try
 	{
 		// Create an instant camera object with the camera device found first.
-		CInstantCamera camera(CTlFactory::GetInstance().CreateFirstDevice());
+		//CInstantCamera camera(CTlFactory::GetInstance().CreateFirstDevice());
 
 		// Print the model name of the camera.
-		cout << "Using device " << camera.GetDeviceInfo().GetFullName()<< ", serial: " << camera.GetDeviceInfo().GetSerialNumber() << endl;
+		//cout << "Using device " << camera.GetDeviceInfo().GetFullName()<< ", serial: " << camera.GetDeviceInfo().GetSerialNumber() << endl;
 
 		// The parameter MaxNumBuffer can be used to control the count of buffers allocated for grabbing. The default value of this parameter is 10.
-		camera.MaxNumBuffer = 5;
+		//camera.MaxNumBuffer = 5;
 
 		// Start the grabbing of c_countOfImagesToGrab images. The camera device is parameterized with a default configuration which sets up free-running continuous acquisition.
-		camera.StartGrabbing(c_countOfImagesToGrab);
+		kamery[0].StartGrabbing(c_countOfImagesToGrab);
 
 		// This smart pointer will receive the grab result data.
 		CGrabResultPtr ptrGrabResult;
 
 		// Camera.StopGrabbing() is called automatically by the RetrieveResult() method when c_countOfImagesToGrab images have been retrieved.
-		while (camera.IsGrabbing())
+		while (kamery[0].IsGrabbing())
 		{
 			// Wait for an image and then retrieve it. A timeout of 5000 ms is used.
-			camera.RetrieveResult(5000, ptrGrabResult, TimeoutHandling_ThrowException);
+			kamery[0].RetrieveResult(5000, ptrGrabResult, TimeoutHandling_ThrowException);
 
 			// Image grabbed successfully?
 			if (ptrGrabResult->GrabSucceeded())
